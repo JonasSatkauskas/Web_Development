ead0fb18973014321efbbc03e0c26b14f548634b
JonasSatkauskas <satkusmedia@gmail.com>
2021-12-07 15:07:12 +0200

    Nesting conditionals

--- JS/Conditionals/app.js
+++ JS/Conditionals/app.js
@@ -26,14 +26,29 @@
 // }
 
 
-const age = 4;
+// const age = 4;
 
-if (age < 5) {
-    console.log('You are a baby. You can go for free')
-} else if (age < 10) {
-    console.log('You are a child. You pay $10')
-} else if (age < 65) {
-    console.log('You are an adult. You pay $20')
+// if (age < 5) {
+//     console.log('You are a baby. You can go for free')
+// } else if (age < 10) {
+//     console.log('You are a child. You pay $10')
+// } else if (age < 65) {
+//     console.log('You are an adult. You pay $20')
+// } else {
+//     console.log('You are a senior. You pay $10')
+// }
+
+
+const password = prompt('Enter a new password')
+
+if (password.length >= 6) {
+    // Check if there is a space
+    if (password.indexOf(' ') === -1) {
+        console.log('Valid password')
+    } else {
+        console.log('Password cannot contain spaces')
+    }
+
 } else {
-    console.log('You are a senior. You pay $10')
+    console.log('PASSWORD TOO SHORT! MUST BE 6+ CHARS')
 }
