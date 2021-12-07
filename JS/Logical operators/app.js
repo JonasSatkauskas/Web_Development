36fa10f7d0a2105efd39978db966b727897f0227
JonasSatkauskas <satkusmedia@gmail.com>
2021-12-07 15:30:34 +0200

    OR operator

--- JS/Logical operators/app.js
+++ JS/Logical operators/app.js
@@ -1,7 +1,20 @@
-const password = prompt('Enter your password')
+// const password = prompt('Enter your password')
 
-if (password.length >= 6 && password.indexOf(' ') === -1) {
-    console.log('VALID PASSWORD')
+// if (password.length >= 6 && password.indexOf(' ') === -1) {
+//     console.log('VALID PASSWORD')
+// } else {
+//     console.log('INCORRECT FORMAT FOR PASSWORD')
+// }
+
+
+const age = 9;
+// AND will run before OR operator
+if (age >= 0 && age < 5 || age >= 65) {
+    console.log('FREE')
+} else if (age >= 5 && age < 10) {
+    console.log('$10')
+} else if (age >= 10 && age < 65) {
+    console.log('$20')
 } else {
-    console.log('INCORRECT FORMAT FOR PASSWORD')
+    console.log('Invalid age')
 }
