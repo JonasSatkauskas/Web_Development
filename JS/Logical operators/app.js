ced3e8dc0555b53f74c3589c44aa1d3006d36e55
JonasSatkauskas <satkusmedia@gmail.com>
2021-12-07 15:39:45 +0200

    NOT operator

--- JS/Logical operators/app.js
+++ JS/Logical operators/app.js
@@ -7,14 +7,20 @@
 // }
 
 
-const age = 9;
-// AND will run before OR operator
-if (age >= 0 && age < 5 || age >= 65) {
-    console.log('FREE')
-} else if (age >= 5 && age < 10) {
-    console.log('$10')
-} else if (age >= 10 && age < 65) {
-    console.log('$20')
-} else {
-    console.log('Invalid age')
+// const age = 9;
+// // AND will run before OR operator
+// if (age >= 0 && age < 5 || age >= 65) {
+//     console.log('FREE')
+// } else if (age >= 5 && age < 10) {
+//     console.log('$10')
+// } else if (age >= 10 && age < 65) {
+//     console.log('$20')
+// } else {
+//     console.log('Invalid age')
+// }
+
+
+const firstName = prompt('Enter your first name');
+if (!firstName) {
+    firstName = prompt('Try again')
 }
